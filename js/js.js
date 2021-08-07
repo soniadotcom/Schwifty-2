cd8e00d7c7a0ba57faad134236288b8d761103e2
Sonia F <64592264+soniadotcom@users.noreply.github.com>
2021-08-07 20:01:27 +0300

    first commit

---
+++ js/js.js
@@ -0,0 +1,23 @@
+$('.responsive').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1300,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      }
+    ]
+});
+
+
+AOS.init({
+    duration: 1200,
+})
